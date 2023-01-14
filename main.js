cfd6f874c09ca5aa578d17f26c18767c273d5211
Stef Baughman <76499574+HonduranCoder@users.noreply.github.com>
2023-01-14 15:52:57 -0800

    Update main.js

--- main.js
+++ main.js
@@ -149,3 +149,196 @@ export function isHappyNumber(n){
     //evaluate to either true or false 
     return n===1;
 }
+
+//Linked List Cycle 
+  //Helper Function
+    //Linked List Node Class
+class LinkedListNode {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+export default LinkedListNode;
+
+  //Helper Function
+    //Printing Linked List with forward arrows 
+function printListWithForwardArrow(linkedListNode) {
+  let temp = linkedListNode;
+  let result = "";
+  i = 0;
+  while (temp !== null && i < 7) {
+    result += temp.data;
+    temp = temp.next;
+
+    if (i === 6){
+      result += " (...)"
+      break
+    }
+
+    if (temp !== null) result += " →  ";
+    // if this is the last node, print null at the end
+
+    else result += " → NULL"
+    
+    i++;
+  }
+  
+  return result;
+}
+
+export default printListWithForwardArrow;
+
+  //index.js 
+import printListWithForwardArrow from "./print_list";
+import LinkedList from "./linked_list";
+
+function detectCycle(head) {
+  var fast, i, j, last, slow, space1, space2;
+  [slow, fast] = [head, head];
+  [i, j] = [0, 0];
+  [space1, space2, last] = [0, 2, 5];
+  console.log("\tHead pointer:", head.data);
+  console.log("\tSlow pointer:", slow.data);
+  console.log("\tFast pointer:", fast.data);
+  console.log("\t" + printListWithForwardArrow(head));
+  console.log("\t" + "  ".repeat(6 * i) + "ŝf̂");
+
+  // Run the loop until we reach the end of the
+  // linked list or find a cycle
+  while (fast != null && fast.next != null) {
+    slow = slow.next;
+    fast = fast.next.next;
+    last -= 2;
+    console.log("\n\tLoop index:", i);
+    i += 1;
+    j += 2;
+
+    // Break the loop if fast pointer has reached at
+    // the end of linked list
+    if (fast === null) {
+      console.log("\tSlow pointer:", slow.data);
+      console.log("\tFast pointer: NULL");
+      console.log("\tFast pointer has reached the end of the linked list.");
+      break;
+    } else {
+      console.log("\tSlow pointer:", slow.data);
+      console.log("\tFast pointer:", fast.data, "\n");
+      console.log("\t" + printListWithForwardArrow(head));
+    }
+
+    if (slow === fast) {
+      console.log("\t" + " ".repeat(6 * i - 4) + "ŝf̂");
+      console.log(
+        "\n\tBoth slow and fast pointers are pointing to the same node!"
+      );
+      return true;
+    } else {
+      if (last === -1) {
+        console.log(
+          "\t" +
+          " ".repeat(6 * (space2 - 1) - (6 * space1 - 1) - 3) +
+          "f̂ast  " +
+          " ".repeat(6 * i - 14) +
+          "ŝlow"
+        );
+        space2 += 2;
+      }
+
+      if (last === -3) {
+        console.log(
+          "\t" +
+          " ".repeat(6 * (space2 - 1) - (6 * space1 - 1) - 5) +
+          "f̂ast " +
+          " ".repeat(6 * i - 12 - 10) +
+          "ŝlow"
+        );
+        space2 += 2;
+      }
+
+      if (last > 0) {
+        console.log(
+          "\t" +
+          " ".repeat(6 * i - (i - 1) - 1) + "ŝlow" +
+          " ".repeat(6 * (j - 1) - 6 * i) +
+          " f̂ast"
+        );
+      }
+    }
+  }
+
+  return false;
+}
+
+function main() {
+  input = [
+    [2, 4, 6, 8, 10, 12],
+    [1, 3, 5, 7, 9, 11],
+    [0, 1, 2, 3, 4, 6],
+    [3, 4, 7, 9, 11, 17],
+    [5, 1, 4, 9, 2, 3],
+  ];
+
+
+
+    for (var i = 0; i < input.length; i++){
+        inputLinkedList = new LinkedList()
+        inputLinkedList.createLinkedList(input[i])
+        if (i % 2 === 0){
+            inputLinkedList.head.next.next.next.next.next.next =
+                        inputLinkedList.head.next
+        }
+        console.log(i + 1 + ".\tInput:")
+        console.log("\t", printListWithForwardArrow(inputLinkedList.head))
+        console.log("\n\tProcessing...")
+
+        var result = detectCycle(inputLinkedList.head) ? "True" : "False"
+        
+        console.log("\n\tDetected cycle =", result)
+        console.log("-".repeat(100), "\n")
+    }
+}
+
+main();
+//Initialize both pointers as head
+//Move both Pointers ar different rates 
+//Check the equality of pointers 
+class LinkedList {
+    constructor() {
+        this.head = null;
+
+        //insertNodeAtHead method will insert a LinkedListNode at head of a linked list.
+        this.insertNodeAtHead = function (node) {
+            if (this.head != null) {
+                node.next = this.head;
+                this.head = node;
+            } else this.head = node;
+        };
+
+        //createLinkedList method will create the linked list using the given integer array with the help of InsertAthead method.
+        this.createLinkedList = function (list) {
+            list.reverse().forEach((element) => {
+                let newNode = new LinkedListNode(element);
+                this.insertNodeAtHead(newNode);
+            });
+        };
+
+        //this method will display the elements of the linked list.
+        this.display = function () {
+            let result = "",
+                temp = this.head;
+            while (temp != null) {
+                result += temp.data;
+                temp = temp.next;
+                if (temp != null) {
+                    result += ", ";
+                }
+            }
+            result += "";
+            return result;
+        };
+    }
+}
+
+export default LinkedList;
