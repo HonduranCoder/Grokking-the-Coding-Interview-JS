9093fb99f76a2725ade619727f3d951ab7b7faa5
Stef Baughman <76499574+HonduranCoder@users.noreply.github.com>
2023-01-15 12:31:12 -0800

    Update main.js

--- main.js
+++ main.js
@@ -307,38 +307,34 @@ main();
 class LinkedList {
     constructor() {
         this.head = null;
-
-        //insertNodeAtHead method will insert a LinkedListNode at head of a linked list.
-        this.insertNodeAtHead = function (node) {
-            if (this.head != null) {
-                node.next = this.head;
-                this.head = node;
-            } else this.head = node;
-        };
-
-        //createLinkedList method will create the linked list using the given integer array with the help of InsertAthead method.
-        this.createLinkedList = function (list) {
-            list.reverse().forEach((element) => {
-                let newNode = new LinkedListNode(element);
-                this.insertNodeAtHead(newNode);
-            });
-        };
-
-        //this method will display the elements of the linked list.
-        this.display = function () {
-            let result = "",
-                temp = this.head;
-            while (temp != null) {
-                result += temp.data;
-                temp = temp.next;
-                if (temp != null) {
-                    result += ", ";
-                }
-            }
-            result += "";
-            return result;
-        };
-    }
+    //insertNodeAtHead method will insert a LinkedListNode at head of linked list
+      this.insertNodeAtHead = function (node) {
+       if (this.head != null){
+         node.next = this.head; 
+         this.head = node;
+      } else this.head = node; 
+    };
+      //createLinkedList method will create the linked list using the given integer array with the help of InsertNodeAtHead method.
+      this.createLinkedList = function (list){
+        list.reverse().forEach((element) => {
+          let newNode = new LinkedListNode(element);
+          this.insertNodeAtHead(newNode);
+        });
+     };
+      //display elements of the linked list
+      this.display = function () {
+       let result = "", 
+           temp = this.head; 
+        while(temp != null) {
+          result += temp.data; 
+          temp = temp.next; 
+          if(temp != null) {
+            result += " , "; 
+          }
+      }
+        result += ""; 
+        return result;
+      };
+   }
 }
-
 export default LinkedList;
