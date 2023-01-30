2b03375aaec1b2c826bdf06fd63904d4a773057b
Stef Baughman <76499574+HonduranCoder@users.noreply.github.com>
2023-01-30 11:33:48 -0800

    Update main.js

--- main.js
+++ main.js
@@ -627,4 +627,22 @@ function palindrome(head) {
   return false;
 }
 
-    
+//Sliding Window 
+export function findMaxSlidingWindow(nums, w) {
+  const res = [];
+  const q = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    while (q.length - 1 >= 0 && nums[i] > q[q.length - 1]) q.pop();
+    q.push(nums[i]);
+
+    //when i + 1 - w >= 0, the window is fully overlapping nums
+    const j = i + 1 - w;
+    if (j >= 0) {
+      res.push(q[0]);
+      //if the biggest element in q is about to exit window, remove it from q
+      if (nums[j] === q[0]) q.shift(); 
+    }
+  }
+  return res;
+};
